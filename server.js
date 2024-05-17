87813c4613489cc9b80851a0b0d957a8ff34fd7f
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2024-05-17 11:18:53 +0530

    firebase credentials added to the api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const server =require("./express")()
 const  {port}  = require('./config');
-
+require('dotenv').config();
 
 server.listen(port, async()=>{
     console.log(`server listening at http://localhost:${port}`)
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,21 @@
 const admin = require("firebase-admin");
-const credentials = require("../../key.json");
 const nodemailer = require('nodemailer')
+require('dotenv').config();
+
+// firebase credentials for portfolio
+const credentials = {
+  "type": process.env.TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.UNIVERSE_DOMAIN
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(credentials),
@@ -31,7 +46,7 @@ const portifiMail = (FullName,Email,Subject,Message) => {
       service: 'gmail',
       auth: {
         user: 'cmfriend111@gmail.com',
-        pass: 'qgpg datn datn edng'
+        pass: process.env.PASSKEY
       }
     });
 
